203b642ef6eaf6097c2ad338346e59a38bbf0a23
brij-mohan09 <it09brijmohan@gmail.com>
2026-03-30 12:07:51 +0530

    fix resume view

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -10,7 +10,7 @@ const Resume = () => {
 
         <div className="resume-actions">
           <a
-            href="/public/resume/Brij_Mohan_React_Frontend_Developer.pdf"
+            href="/resume/Brij_Mohan_React_Frontend_Developer.pdf"
             download
             className="resume-download"
           >
@@ -20,7 +20,7 @@ const Resume = () => {
 
         <div className="resume-viewer">
           <iframe
-            src="/public/resume/Brij_Mohan_React_Frontend_Developer.pdf"
+            src="/resume/Brij_Mohan_React_Frontend_Developer.pdf"
             title="Resume PDF"
             loading="lazy"
           />
